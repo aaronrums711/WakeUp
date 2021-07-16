ae1137c88050a5b1e2c9ba142d95596f065a9dee
Aaron Rumley <rum01867@Admiral.local>
2021-07-16 06:02:33 -0400

    continuing work on pool stick scripts

--- DreamGame/Assets/Scripts/MiniGames/PoolBilliards/PoolStick.cs
+++ DreamGame/Assets/Scripts/MiniGames/PoolBilliards/PoolStick.cs
@@ -4,25 +4,77 @@ using UnityEngine;
 
 public class PoolStick : MiniGame
 {
-    public GameObject target;
-    private float angle = 20; //controls speed. this is arbitrary, since we are using rotationSpeed to control it as well. 
+
+
+    //////////////////////////////Config
+    private float angle = 20; //controls rotation speed around ball. this is arbitrary, since we are using rotationSpeed to control it as well. 
     public float rotationSpeed;
     public float drawBackSpeed;
+    public float hitSpeed = 10;
+    private float pullBackDistance;
+
+    //////////////////////////////State
+    public bool isDrawingBack = false;
+
+    //////////////////////////////Cached Component References
+    public GameObject ball;
+    public Rigidbody2D thisRB;
+
+
+
 
     void Start()
     {
         orderInLevel = 1;
+        thisRB = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if (!Input.GetKey(keysToPlay[orderInLevel]))
+        if (!Input.GetKey(keysToPlay[orderInLevel])  && isDrawingBack == false)
         {
-            transform.RotateAround(target.transform.position, Vector3.forward, angle * Time.deltaTime * rotationSpeed);
+            transform.RotateAround(ball.transform.position, Vector3.forward, angle * Time.deltaTime * rotationSpeed);
         }
-        else if (Input.GetKey(keysToPlay[orderInLevel]))
+        else if (Input.GetKeyDown(keysToPlay[orderInLevel]))
         {
-            transform.Translate(Vector2.down * Time.deltaTime * drawBackSpeed);
+            StartCoroutine(InitiatePullBack());
+        }
+    }
+
+    private IEnumerator InitiatePullBack()
+    {
+        isDrawingBack = true;
+        float maxPullBackDistance = 2f;
+        Vector2 pullBackStartPos = this.transform.position;
+        pullBackDistance = Vector2.Distance(new Vector2(transform.position.x, transform.position.y),pullBackStartPos);
+
+        while(Input.GetKey(keysToPlay[orderInLevel]) && pullBackDistance < maxPullBackDistance)  
+        {
+            transform.Translate(Vector2.down  * Time.deltaTime * drawBackSpeed);
+            pullBackDistance = Vector2.Distance(new Vector2(transform.position.x, transform.position.y),pullBackStartPos);
+            yield return null;
+        }
+
+        Debug.Log("final pull back distance: " + pullBackDistance);
+      
+        CueHit();
+    }
+
+
+    public void CueHit()
+    {
+        thisRB.velocity = Vector2.up + new Vector2(0, hitSpeed * pullBackDistance);
+        //push stick forward, hitting ball.  speed will use pull back distance. 
+        //stick will draw back slightly after hit.
+        //stick will fade away
+        //stick will reappear when ball stops moving. 
+    }
+
+    void OnCollisionEnter2D(Collision other)
+    {
+        if (other.gameObject.name == "Ball")
+        {
+            thisRB.velocity = Vector2.zero;
         }
     }
 }
