ff41c57c9d8781e7c22efecaa6280b85fe2535fd
Aaron Rumley <rum01867@Admiral.local>
2021-07-17 19:16:59 -0400

    starting manager script

--- DreamGame/Assets/Scripts/MiniGames/PoolBilliards/PoolBilliardsManager.cs
+++ DreamGame/Assets/Scripts/MiniGames/PoolBilliards/PoolBilliardsManager.cs
@@ -12,7 +12,7 @@ using UnityEngine;
 public class PoolBilliardsManager : MiniGameElement
 {
     [SerializeField] private int targetsInPlay;
-
+    [SerializeField] private int maxTargetsAllowed = 4;
 
     
     void Start()
@@ -33,4 +33,31 @@ public class PoolBilliardsManager : MiniGameElement
     //UPON RETURN: create a method to spawn target balls periodically.  
     //delete the old layers and create new ones.  I think I like the idea of target balls being able to knock out other target balls, 
     //so that layer should be able to collide with itself. 
+
+    //for spawning logic:
+    //for every ball less than the max amount, there is a 50% chance to spawn one, then a 40% chance to spawn the next...etc. 
+    //this makes it likely that at least one ball will spawn,  but unlikely for all of them up to the max to spawn
+
+    //attempt to spawn for each missing ball up to the max.  the chance of getting one to spawn decreases with each.  
+    private void AttemptToSpawnTargets()
+    {
+        float spawnChance= 0.5f;
+        float spawnChanceReduction = 0.1f;
+        for (int i=maxTargetsAllowed-targetsInPlay; i>=0; i--)
+        {
+            float chance = UnityEngine.Random.Range(0f, 1f);
+            if (chance > spawnChance)
+            {
+                //spawn target
+                return;
+            }
+            spawnChance -= spawnChanceReduction;
+        }
+    }
+
+    private void SpawnTarget()
+    {
+        return;
+        //need to search for a spot somehow, then spawn the target ball at that spot.  
+    }
 }
