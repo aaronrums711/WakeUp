9e3c71d57c30677ee55b1beb4a725b0c3eaa21f0
Aaron Rumley <rum01867@Admiral.local>
2021-07-17 23:25:41 -0400

    fixing pool manager script

--- DreamGame/Assets/Scripts/MiniGames/PoolBilliards/PoolBilliardsManager.cs
+++ DreamGame/Assets/Scripts/MiniGames/PoolBilliards/PoolBilliardsManager.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 //this class is mostly just responsible for respawning targets.  There needs to be enough targets at a time so that the player 
 //always has something to aim at.  
 
-//either...
-//it can spit out balls periodically.  the more balls that are in play should  mean that it's less likley for one to appear. We don't want it to get crowded. 
-
-//or, they can be spit out only when some balls are hit. 
 public class PoolBilliardsManager : MiniGameElement
 {
+
+
+    //////////////////////////////Config
+    public float targetLocationPadding = 1f;
+
+    //////////////////////////////State
+
+    //////////////////////////////Cached Component References
     [SerializeField] private int targetsInPlay;
     [SerializeField] private int maxTargetsAllowed = 4;
-
+    public GameObject targetBallPrefab;
+    public Transform targetBallParent;
     
     void Start()
     {
         GetTargetCount();
+        // AttemptToSpawnTargets();
     }
 
     void Update()
@@ -34,27 +40,22 @@ public class PoolBilliardsManager : MiniGameElement
     //delete the old layers and create new ones.  I think I like the idea of target balls being able to knock out other target balls, 
     //so that layer should be able to collide with itself. 
 
-    //for spawning logic:
-    //for every ball less than the max amount, there is a 50% chance to spawn one, then a 40% chance to spawn the next...etc. 
-    //this makes it likely that at least one ball will spawn,  but unlikely for all of them up to the max to spawn
-
-    //attempt to spawn for each missing ball up to the max.  the chance of getting one to spawn decreases with each.  
-    //however, if there are Max-1 balls in play, then there should be a slim chance of spawning another one, since there are already others to hit
+    [ContextMenu("attempt to spawn target")]
     private void AttemptToSpawnTargets()
     {
         if (targetsInPlay == maxTargetsAllowed) {return;}
 
-        int attempts = maxTargetsAllowed-targetsInPlay;
-
+        float attempts = maxTargetsAllowed-targetsInPlay;
         float spawnChance= 1f * (attempts/maxTargetsAllowed);  //if there's Max-1 targets in play, there will only be a 20% chance of spawning another
         float spawnChanceReduction = 0.1f;
-        for (int i=attempts; i>=0; i--)
+        for (float i=attempts; i>0; i--)
         {
             float chance = UnityEngine.Random.Range(0f, 1f);
-            if (chance > spawnChance)
+            print("spawn threshold: " + spawnChance);
+            print("spawn attempt: " + chance);
+            if (chance < spawnChance)
             {
-                //spawn target
-                return;
+                SpawnTarget();
             }
             spawnChance -= spawnChanceReduction;
         }
@@ -62,9 +63,34 @@ public class PoolBilliardsManager : MiniGameElement
 
     private void SpawnTarget()
     {
-        return;
-        //need to search for a spot somehow, then spawn the target ball at that spot.  
+        Instantiate(targetBallPrefab, SearchForLocation(), Quaternion.identity, targetBallParent);
+        GetTargetCount();
     }
 
-    ///UPON RETURN: find a way to randomly spawn targets within the play area .  I think the AttemptToSpawnTargets method is good 
+
+    private Vector3 SearchForLocation()
+    {
+        float maxY = 0;
+        float maxX=  0;
+        float minY = 0;
+        float minX = 0;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            float testY =  this.transform.GetChild(i).transform.position.y;
+            float testX = this.transform.GetChild(i).transform.position.x;
+            if (testY > maxY) {maxY = testY-targetLocationPadding;}
+            else if(testY< minY){minY = testY+targetLocationPadding;}
+
+            if (testX > maxX) {maxX = testX-targetLocationPadding;}
+            else if(testX < minX) {minX = testX+targetLocationPadding;}
+        }
+
+        Vector3 targetBallLocation = new Vector3(Random.Range(minY, maxY), Random.Range(minX, maxX));
+        // print("maxY: " + maxY);
+        // print("maxX: " + maxX);
+        // print("minY: " + minY);
+        // print("minX: " + minX);
+        return targetBallLocation;
+
+    }
 }
