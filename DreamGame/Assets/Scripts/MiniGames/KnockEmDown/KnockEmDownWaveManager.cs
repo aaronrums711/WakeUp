9037742042e5e768175d09cbb4b19ea07bdb7e8d
aaronrums <aaron.rumley@elephant.com>
2021-07-23 11:01:39 -0400

    made changes to wave manager.
    Functionality is now complete such that a wave with a semi-random amount of targets is spawned with a single method call.

---
+++ DreamGame/Assets/Scripts/MiniGames/KnockEmDown/KnockEmDownWaveManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+    /**
+        this should create a wave every 8-10 seconds maybe.  The
+        Then, each wave gets a semi-random number of targets spawned.  
+    **/
+public class KnockEmDownWaveManager : MiniGameElement
+{
+    //////////////////////////////Config
+    public int minWaveAmount;
+    public int maxWaveAmount;
+
+    [Range(0.1f, 2f)]  public float timeBetweenEachSpawn;
+    //////////////////////////////State
+    public bool isCurrentWaveOnPlayArea = false;
+
+    //////////////////////////////Cached Component References
+    KnockDownTargetSpawner spawner;
+
+
+
+    void Start()
+    {
+        spawner = parentMiniGame.GetComponentInChildren<KnockDownTargetSpawner>();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator SpawnWave(int min, int max)
+    {
+        if (isCurrentWaveOnPlayArea)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        isCurrentWaveOnPlayArea = true;
+        int targetsInWave = Random.Range(min, max+1);
+        for(int i=0; i < targetsInWave; i++)
+        {
+            spawner.InstantiateTarget();
+            yield return new WaitForSeconds(timeBetweenEachSpawn);
+        }
+        isCurrentWaveOnPlayArea = false;
+    }
+    [ContextMenu("SpawnWaveFromEditor()")]
+    public void SpawnWaveFromEditor()
+    {
+        StartCoroutine(SpawnWave(minWaveAmount, maxWaveAmount));
+    }
+}
