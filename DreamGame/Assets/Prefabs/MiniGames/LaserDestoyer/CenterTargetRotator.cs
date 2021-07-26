7d461c0ef081bbf149d79ddfd94f077f356eaaa0
Aaron Rumley <rum01867@Admiral.local>
2021-07-26 09:58:39 -0400

    I Think the rotation is better now

--- DreamGame/Assets/Prefabs/MiniGames/LaserDestoyer/CenterTargetRotator.cs
+++ DreamGame/Assets/Prefabs/MiniGames/LaserDestoyer/CenterTargetRotator.cs
@@ -23,14 +23,32 @@ public class CenterTargetRotator : MonoBehaviour
     {
         // this.transform.LookAt(rotationTesting.position, Vector3.forward);
         // this.transform.LookAt(rotationTesting.position, Vector3.right);
-        this.transform.right = rotationTesting.position;
+        // this.transform.right = rotationTesting.position;
 
         // this.transform.Rotate(rot *Time.deltaTime);
     }
 
-    public void RotateChunk()
-    {
 
+
+    public IEnumerator RotateChunk()
+    {   
+        float rotationAmount = 45f * (Random.Range(1,5));
+        float targetRotation = this.transform.rotation.eulerAngles.z + rotationAmount > 360 ? this.transform.rotation.eulerAngles.z + rotationAmount -360 : this.transform.rotation.eulerAngles.z + rotationAmount;
+        print("target rotation: " + targetRotation);
+        while (Mathf.Abs(this.transform.rotation.eulerAngles.z - targetRotation) > 2)
+        {
+            this.transform.Rotate(rot *Time.deltaTime);
+            print(this.transform.rotation.eulerAngles.z);
+            yield return null;
+        }
+        // this.transform.rotation.eulerAngles = new Vector3(0,0,targetRotation);
+    }
+
+
+    [ContextMenu("RotateChunk()")]
+    private void RotateChunkFromEditor()
+    {
+        StartCoroutine(RotateChunk());
     }
 
 }
