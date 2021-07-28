dda7ba0a8d7d1b7b83b8f1de0a1f9ca3cf62ba1d
Aaron Rumley <rum01867@Admiral.local>
2021-07-28 07:05:48 -0400

    problem fixed!
    script was using transform.position instead of the correct value

--- DreamGame/Assets/Prefabs/MiniGames/LaserDestoyer/LaserEmitter.cs
+++ DreamGame/Assets/Prefabs/MiniGames/LaserDestoyer/LaserEmitter.cs
@@ -23,18 +23,18 @@ public class LaserEmitter : MonoBehaviour
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 2;
         lineRenderer.enabled = true;
-        // StartCoroutine(InitialLaserCast());
+        StartCoroutine(InitialLaserCast());
         print("starting line position count: " + lineRenderer.positionCount);
 
     }
 
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(emissionPoint.position, centerTarget.position- emissionPoint.position);
-        Debug.DrawLine(emissionPoint.position, hit.point);
-        lineRenderer.SetPosition(0, emissionPoint.position);
-        lineRenderer.SetPosition(1, hit.point);
-        // lineRenderer.SetPosition(2, new Vector3(5,5,5));
+        // RaycastHit2D hit = Physics2D.Raycast(emissionPoint.position, centerTarget.position- emissionPoint.position);
+        // Debug.DrawLine(emissionPoint.position, hit.point);
+        // lineRenderer.SetPosition(0, emissionPoint.position);
+        // lineRenderer.SetPosition(1, hit.point);
+        // // lineRenderer.SetPosition(2, new Vector3(5,5,5));
     }
 
 
@@ -47,14 +47,14 @@ public class LaserEmitter : MonoBehaviour
         
         lineRenderer.positionCount= 1;
 
-        RaycastHit2D hit = Physics2D.Raycast(emissionPoint.position, centerTarget.position- this.transform.position);
+        RaycastHit2D hit = Physics2D.Raycast(emissionPoint.position, centerTarget.position- emissionPoint.position);
         Vector2 newPos = Vector2.Lerp((Vector2)emissionPoint.position, (Vector2)hit.point, elapsedPercent);
         lineRenderer.SetPosition(0, newPos);
 
         while (Vector2.Distance(lineRenderer.GetPosition(lineRenderer.positionCount-1), hit.point) > 0.2f)
         {
             lineRenderer.positionCount++;
-            hit = Physics2D.Raycast(emissionPoint.position, centerTarget.position- this.transform.position);
+            hit = Physics2D.Raycast(emissionPoint.position, centerTarget.position- emissionPoint.position);
             elapsedPercent = (Time.time - startTime)/laserInitializationLength;
             nextPosIndex = lineRenderer.positionCount-1;
             newPos = Vector2.Lerp((Vector2)emissionPoint.position, (Vector2)hit.point, elapsedPercent);
