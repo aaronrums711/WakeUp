bdb1919ff33fbfad65b725ae30d85fb9b5ffa3fa
Aaron Rumley <rum01867@Admiral.local>
2021-07-31 06:18:50 -0400

    adding script to control anims

---
+++ DreamGame/Assets/Scripts/MiniGames/LaserDestoyer/LaserDestroyerRingChanger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserDestroyerRingChanger : MonoBehaviour
+{
+	/*****************
+	CreateDate: 7/31/21
+	Functionality: this only triggers animations.  these animations shift around the sizes of the protecive ring edges, for some added fun
+	******************/
+	
+	//////////////////////////////Config
+	
+	//////////////////////////////State
+	
+	//////////////////////////////Cached Component References
+	
+	
+	void Awake()
+    {
+        
+    }
+	
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
