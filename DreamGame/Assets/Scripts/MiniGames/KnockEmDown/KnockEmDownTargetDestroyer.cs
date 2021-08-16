6fee74eec63c378668b4788291c2dc51c0df2b5d
Aaron Rumley <rum01867@Admiral.local>
2021-08-16 08:05:00 -0400

    few more small changes to knockEmDown

--- DreamGame/Assets/Scripts/MiniGames/KnockEmDown/KnockEmDownTargetDestroyer.cs
+++ DreamGame/Assets/Scripts/MiniGames/KnockEmDown/KnockEmDownTargetDestroyer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KnockEmDownTargetDestroyer : MiniGameElement, IProgressionAdder
+public class KnockEmDownTargetDestroyer : MiniGameElement, IProgressionAdder, IOneKeyPlay
 {
     //////////////////////////////Config
      [Tooltip("only used for the brief time that the target grows before it shrinks and disappears")]  
@@ -24,20 +24,25 @@ public class KnockEmDownTargetDestroyer : MiniGameElement, IProgressionAdder
     {
         if (Input.GetKeyDown(parentMiniGame.keyForThisGame) && parentMiniGame.isActive)
         {
-            if (waveManager.objectsInCurrentWave.Count <= 0)
-            {
-                parentMiniGame.AddProgress(-0.02f);
-            }
-            else if(waveManager.objectsInCurrentWave.Count >= 0)
-            {
-                AddMiniGameProgress();
-            }
+            OneKeyPlay();
         }
     }
 
     public void AddMiniGameProgress()
     {
         float scale = waveManager.objectsInCurrentWave[0].GetComponent<KnockEmDownTarget>().CallDestroyEffect();
-        parentMiniGame.AddProgress(parentMiniGame.baseProgression * scale);
+        parentMiniGame.AddProgress(parentMiniGame.baseProgressionChunk * scale);
+    }
+
+    public void OneKeyPlay()
+    {
+        if (waveManager.objectsInCurrentWave.Count <= 0)
+        {
+            parentMiniGame.AddProgress(-0.02f);
+        }
+        else if(waveManager.objectsInCurrentWave.Count >= 0)
+        {
+            AddMiniGameProgress();
+        }  
     }
 }
