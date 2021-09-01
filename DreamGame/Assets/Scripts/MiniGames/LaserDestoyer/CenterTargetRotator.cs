95e30ddbdc8b739eb82c14bcf9f8bf35412368a2
Aaron Rumley <rum01867@Admiral.local>
2021-09-01 06:03:37 -0400

    renamed a few things
    trying to get the stop method down

--- DreamGame/Assets/Scripts/MiniGames/LaserDestoyer/CenterTargetRotator.cs
+++ DreamGame/Assets/Scripts/MiniGames/LaserDestoyer/CenterTargetRotator.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class CenterTargetRotator : MiniGameElement
 {
     //////////////////////////////Config
-    public Vector3 baseRot;
-    public Vector3 rotPlusRandom;
+    public Vector3 baseRotSpeed;
+    public Vector3 rotSpeedPlusRandom;
+    public float targetRotation;
     //////////////////////////////State
     //////////////////////////////Cached Component References
 
@@ -19,13 +20,13 @@ public class CenterTargetRotator : MiniGameElement
     public IEnumerator RotateChunk()
     {   
         float rotationAmount = 45f * (Random.Range(1,7));
-        float targetRotation = this.transform.rotation.eulerAngles.z + rotationAmount > 360 ? this.transform.rotation.eulerAngles.z + rotationAmount -360 : this.transform.rotation.eulerAngles.z + rotationAmount;
+        targetRotation = this.transform.rotation.eulerAngles.z + rotationAmount > 360 ? this.transform.rotation.eulerAngles.z + rotationAmount -360 : this.transform.rotation.eulerAngles.z + rotationAmount;
         int OneOrZero = Random.Range(0,2);
-        rotPlusRandom = baseRot * Random.Range(0.5f, 3f);   //as of now, the rotator only rotates in one direction, to match the counter clockwise direction of the passing turrets.  If you want have it    
+        rotSpeedPlusRandom = baseRotSpeed * Random.Range(0.5f, 3f);   //as of now, the rotator only rotates in one direction, to match the counter clockwise direction of the passing turrets.  If you want have it    
                                                         //rotate randomly in both directions, change this to Random.Range(-3f, 3f);  
         while (Mathf.Abs(this.transform.rotation.eulerAngles.z - targetRotation) > 2)
         {
-            this.transform.Rotate(rotPlusRandom *Time.deltaTime ) ; 
+            this.transform.Rotate(rotSpeedPlusRandom *Time.deltaTime ) ; 
             yield return null;
         }
         this.transform.rotation = Quaternion.Euler(0,0,targetRotation);
@@ -38,5 +39,6 @@ public class CenterTargetRotator : MiniGameElement
         yield return new WaitForSeconds(Random.Range(2,4));
         StartCoroutine(ContinuallyRotate());
     }
+
 
 }
