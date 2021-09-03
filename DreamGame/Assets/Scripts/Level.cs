520490cb80420825a037cc9e1637d4fa90f03bd6
Aaron Rumley <rum01867@Admiral.local>
2021-09-03 08:51:16 -0400

    created level script

---
+++ DreamGame/Assets/Scripts/Level.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new level", menuName = "level")]
+public class Level : ScriptableObject
+{
+	/*****************
+	CreateDate: 	9/3/21
+	Functionality:	contains methods and attributes for each available level option. 
+	Notes:			The best way to implement this is for the MiniGame in each mini game to take it in, then have the MiniGame class use these variables
+					Then, all the subcomponents will look to the MiniGame class. 
+	Dependencies:
+	******************/
+	
+
+		
+
+}
