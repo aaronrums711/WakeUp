6a24d99dba17bc25b7825923fc543176d4333b04
Aaron Rumley <rum01867@Admiral.local>
2021-11-07 07:49:57 -0500

    big commit
    dealing with issues integrating the inner bar func into all mini games

--- DreamGame/Assets/Scripts/InnerBarLeftRight.cs
+++ DreamGame/Assets/Scripts/InnerBarLeftRight.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InnerBarLeftRight : MonoBehaviour
+public class InnerBarLeftRight : MonoBehaviour, IInnerBar
 {
 	/*****************
 	CreateDate: 	11/15/21
--- DreamGame/Assets/Scripts/InnerBarUpDown.cs
+++ DreamGame/Assets/Scripts/InnerBarUpDown.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InnerBarUpDown : MonoBehaviour
+public class InnerBarUpDown : MonoBehaviour, IInnerBar
 {
 	/*****************
 	CreateDate: 	11/15/21
