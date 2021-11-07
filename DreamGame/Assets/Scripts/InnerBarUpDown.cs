83cd651de22a9f61b837aed475db30c2ad17ba2e
Aaron Rumley <rum01867@Admiral.local>
2021-11-07 07:54:52 -0500

    Revert "big commit"
    This reverts commit 6a24d99dba17bc25b7825923fc543176d4333b04.

--- DreamGame/Assets/Scripts/InnerBarUpDown.cs
+++ DreamGame/Assets/Scripts/InnerBarUpDown.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InnerBarUpDown : MonoBehaviour, IInnerBar
+public class InnerBarUpDown : MonoBehaviour
 {
 	/*****************
 	CreateDate: 	11/15/21
