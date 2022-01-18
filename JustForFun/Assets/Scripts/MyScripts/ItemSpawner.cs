a80eb053ff736991e27b7b004e644d24818f1c49
aaronrums <aaron.rumley@elephant.com>
2022-01-18 16:40:33 -0500

    setting up scene

--- JustForFun/Assets/Scripts/MyScripts/ItemSpawner.cs
+++ JustForFun/Assets/Scripts/MyScripts/ItemSpawner.cs
@@ -14,10 +14,13 @@ public class ItemSpawner : MonoBehaviour
 	//////////////////////////////Config
 	public Transform objectParent;
 	public GameObject itemToSpawn;
+	public Transform startingPosition;
 	public float YSpacing;
 	public float XSpacing;
 	public int numColumns;
 	public int numRows;
+
+	private Vector3 lastSpawn;
 
 	//////////////////////////////State
 	
@@ -26,7 +29,8 @@ public class ItemSpawner : MonoBehaviour
 	
 	void Awake()
     {
-        
+		lastSpawn = startingPosition.position;
+        SpawnItems();
     }
 	
     void Start()
@@ -41,6 +45,33 @@ public class ItemSpawner : MonoBehaviour
 
 	public void SpawnItems()
 	{
+		//destroy any stuff that was made in debug
+		for(int i = 0; i < objectParent.childCount; i++)
+		{
+			Destroy(objectParent.GetChild(i));
+		}
+
+
+		for (int x = 0; x < numColumns; x++)
+		{
+			for (int y=0; y < numRows; y++)
+			{
+				Vector3 spawnPos;
+				if (y == 0)
+				{
+					spawnPos = startingPosition.position;
+				}
+				else 
+				{
+					float newX = lastSpawn.x + XSpacing;
+					float newY = lastSpawn.y + YSpacing;
+					spawnPos = new Vector3(newX, newY, 0);
+				}
+				Instantiate(itemToSpawn, spawnPos, Quaternion.identity);
+				
+			}
+		}
+
 
 	}
 }
