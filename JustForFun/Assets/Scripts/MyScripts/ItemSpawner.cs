fb30aeed0e1e889eef3e536e2d333ff8ddc13b4e
aaronrums <aaron.rumley@elephant.com>
2022-01-18 16:27:47 -0500

    new project just for testing/to dust off

---
+++ JustForFun/Assets/Scripts/MyScripts/ItemSpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+	/*****************
+	CreateDate: 	1/18/22
+	Functionality:
+	Notes:
+	Dependencies:
+	******************/
+	
+	//////////////////////////////Config
+	public Transform objectParent;
+	public GameObject itemToSpawn;
+	public float YSpacing;
+	public float XSpacing;
+	public int numColumns;
+	public int numRows;
+
+	//////////////////////////////State
+	
+	//////////////////////////////Cached Component References
+	
+	
+	void Awake()
+    {
+        
+    }
+	
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+	public void SpawnItems()
+	{
+
+	}
+}
---
+++ JustForFun/Assets/Scripts/MyScripts/_Manager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Manager : MonoBehaviour
+{
+	/*****************
+	CreateDate:      1/18/2022
+	Functionality:
+	Notes:
+	Dependencies:
+	******************/
+	
+	//////////////////////////////Config
+	
+	//////////////////////////////State
+	
+	//////////////////////////////Cached Component References
+	
+	public GameObject item;
+
+	
+	void Awake()
+    {
+        
+    }
+	
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+}
