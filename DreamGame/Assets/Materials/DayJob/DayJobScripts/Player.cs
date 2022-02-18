858de6878658612c5e5d3a32877a60cdb9deb4fe
Aaron Rumley <rum01867@Admiral.local>
2022-02-17 22:54:06 -0500

    moved some stuff around

---
+++ DreamGame/Assets/Materials/DayJob/DayJobScripts/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	/*****************
+	CreateDate: 	2/17/22
+	Functionality:	main player class
+	Notes:
+	Dependencies:
+	******************/
+	
+	//////////////////////////////Config
+	int movementSpeed;
+
+	//////////////////////////////State
+	
+	//////////////////////////////Cached Component References
+	
+	
+	void Awake()
+    {
+        
+    }
+	
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
